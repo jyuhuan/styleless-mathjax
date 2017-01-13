4a6ee36019de30086a54895afc680e256dfdee22
Yuhuan Jiang <jyuhuan@users.noreply.github.com>
2017-01-12 21:14:38 -0500

    + sample macros
    Partially ported some macros from styleless-latex for testing purposes

---
+++ styleless.js
@@ -0,0 +1,65 @@
+MathJax.Hub.Config({
+  TeX: {
+    Macros: {
+
+      It: ["\\mathit{#1}", 1],
+
+      calA: "\\mathcal{A}",
+      calB: "\\mathcal{B}",
+      calC: "\\mathcal{C}",
+      calD: "\\mathcal{D}",
+      calE: "\\mathcal{E}",
+      calF: "\\mathcal{F}",
+      calG: "\\mathcal{G}",
+      calH: "\\mathcal{H}",
+      calI: "\\mathcal{I}",
+      calJ: "\\mathcal{J}",
+      calK: "\\mathcal{K}",
+      calL: "\\mathcal{L}",
+      calM: "\\mathcal{M}",
+      calN: "\\mathcal{N}",
+      calO: "\\mathcal{O}",
+      calP: "\\mathcal{P}",
+      calQ: "\\mathcal{Q}",
+      calR: "\\mathcal{R}",
+      calS: "\\mathcal{S}",
+      calT: "\\mathcal{T}",
+      calU: "\\mathcal{U}",
+      calV: "\\mathcal{V}",
+      calW: "\\mathcal{W}",
+      calX: "\\mathcal{X}",
+      calY: "\\mathcal{Y}",
+      calZ: "\\mathcal{Z}",
+
+
+      rmA: "\\mathrm{A}",
+      rmB: "\\mathrm{B}",
+      rmC: "\\mathrm{C}",
+      rmD: "\\mathrm{D}",
+      rmE: "\\mathrm{E}",
+      rmF: "\\mathrm{F}",
+      rmG: "\\mathrm{G}",
+      rmH: "\\mathrm{H}",
+      rmI: "\\mathrm{I}",
+      rmJ: "\\mathrm{J}",
+      rmK: "\\mathrm{K}",
+      rmL: "\\mathrm{L}",
+      rmM: "\\mathrm{M}",
+      rmN: "\\mathrm{N}",
+      rmO: "\\mathrm{O}",
+      rmP: "\\mathrm{P}",
+      rmQ: "\\mathrm{Q}",
+      rmR: "\\mathrm{R}",
+      rmS: "\\mathrm{S}",
+      rmT: "\\mathrm{T}",
+      rmU: "\\mathrm{U}",
+      rmV: "\\mathrm{V}",
+      rmW: "\\mathrm{W}",
+      rmX: "\\mathrm{X}",
+      rmY: "\\mathrm{Y}",
+      rmZ: "\\mathrm{Z}"
+
+
+    }
+  }
+});
